ba800a89c0ced2189e3837548d64da1082681114
erasedbeyond <shivendra22031996@gmail.com>
2020-08-28 01:07:30 +0530

    commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import IPod from './IPod'
+
+
+function App() {
+  return (
+    <div className="App">
+      <IPod/>
+      
+    </div>
+  );
+}
+
+export default App;
+
